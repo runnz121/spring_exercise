69c4e695ff1c26894c37ba46e23b3a6dd44c2a06
박종빈 <runnz121@gmail.com>
2024-06-01 17:01:33 +0900

    [feat] summingint 테스트

---
+++ src/test/java/com/kuku/exercise/exe/StreamTest.java
@@ -0,0 +1,38 @@
+package com.kuku.exercise.exe;
+
+import com.kuku.exercise.domain.TestEntity;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class StreamTest {
+
+    @Test
+    void summingIntTest() {
+
+        TestEntity testEntity1 = TestEntity.builder()
+                .name("testEntity1")
+                .totalQuantity(2)
+                .build();
+
+        TestEntity testEntity2 = TestEntity.builder()
+                .name("testEntity2")
+                .totalQuantity(3)
+                .build();
+
+        List<TestEntity> list = List.of(testEntity1, testEntity2);
+
+        Map<String, Integer> map = list.stream().collect(Collectors.groupingBy(TestEntity::getName,
+                Collectors.summingInt(TestEntity::getTotalQuantity)));
+
+        assertAll(
+                () -> assertThat(map.get("testEntity1")).isEqualTo(2),
+                () -> assertThat(map.get("testEntity2")).isEqualTo(3)
+        );
+    }
+}
